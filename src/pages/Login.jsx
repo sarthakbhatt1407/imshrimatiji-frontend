e76d955a42e479bdcf2c2d2b14b883dfb3754d68
Sarthak Bhatt <sarthakbhatt1407@gmail.com>
2024-02-20 13:25:04 +0530

    added register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,56 +1,7 @@
-// import React from "react";
-// import styled from "styled-components";
-// import login from "../assets/images/background/login.png";
-// import AccountBox from "../component/Login/accountBox/index.jsx";
-// import Navbar from "../component/Header.js/Navbar/Navbar.jsx";
-// const MainBox = styled.div`
-//   background: url(${login});
-//   background-position: center;
-//   background-repeat: no-repeat;
-//   background-size: contain;
-//   height: 93vh;
-//   display: grid;
-//   grid-template-columns: 1fr 1fr;
-//   align-items: center;
-
-//   @media only screen and (max-width: 949px) {
-//     background: white;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//   }
-// `;
-// const EmptyDiv = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const PcEmptyLogin = styled.div`
-//   /* background-color: red; */
-// `;
-
-// const Login = () => {
-//   return (
-//     <>
-//       <Navbar />
-//       <MainBox>
-//         <EmptyDiv>
-//           <AccountBox />
-//         </EmptyDiv>
-//         <PcEmptyLogin></PcEmptyLogin>
-//       </MainBox>
-//     </>
-//   );
-// };
-
-// export default Login;
-
 import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import Navbar from "../component/Header.js/Navbar/Navbar";
 import Footer from "../component/Footer/Footer";
-import AccountBox from "../component/Login/accountBox";
 import { colors } from "../data";
 import CompLoader from "../component/Loaders/CompLoader/CompLoader";
 import { useNavigate } from "react-router-dom";
@@ -216,7 +167,7 @@ const LoaderBox = styled.div`
   left: 0;
   border-radius: 0.6rem;
   z-index: 2;
-  background-color: #ebeaea53;
+  background-color: #f9f8f852;
 `;
 
 const Input = styled.input`
